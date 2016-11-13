d63ee4799886d34e533cf935faef08fd10dae1ec
Niklas <n.schnierstein@stud.uni-goettingen.de>
2016-11-13 16:52:57 +0100

    finished ex3

--- ex3_duffing/duffing2.py
+++ ex3_duffing/duffing2.py
@@ -8,7 +8,8 @@ X = np.linspace(-grid,grid,N)
 
 w0 = np.dstack(np.meshgrid(X, X)).reshape(-1, 2)
 
-gamma = 0.1
+# for going backwards in time use negative gamma
+gamma = 0.5
 
 t = np.linspace(0.,50.,100)
 
@@ -17,12 +18,21 @@ def f(w,t):
 
     return [y, -1*gamma*y+x-x**3]
 
+def g(w,t):
+    x,y = w
+    return [-y, gamma*y-x+x**3]
 
 wsol = odeint(f,w0[0],t)
 
-sol = np.array([[w0[0][0],w0[0][1],-1]])
-
+sol = np.array([[w0[0][0],w0[0][1],wsol[-1][0]]])
+k = 1
+p = 1
 for point in w0[1:]:
+
+    if (k/1000 == p):
+        print k
+        p = p+1
+    k = k+1
     wsol = odeint(f,point,t)
 
     if ((wsol[-1][0] <= -0.99) & (wsol[-1][0] >= -1.01)):
@@ -36,13 +46,33 @@ for point in w0[1:]:
 Z = np.array([e[2] for e in sol])
 
 print str(len(Z)) + ' Punkte wurden berechnet.'
+
+eigenvector = np.array([0.5*(gamma-np.sqrt(4+gamma**2)),1])
+
+startpoints = [0.05*eigenvector, 0.05*(-1)*eigenvector]
+print startpoints
+t2 = np.linspace(0.,20.,1000)
 plt.figure(1)
+j = 0
+colors = ['green', 'cyan']
+for startpoint in startpoints:
+    wsol = odeint(g,startpoint,t2)
+    plt.plot([e[0] for e in wsol],[e[1] for e in wsol],c = colors[j],linewidth = 5, label = 'stable manifold')
+    j=j+1
+
+############    PLOTTEN     ####################
+
 XX, YY = np.meshgrid(X,X)
+
+sx = [e[0] for e in startpoints]
+sy = [e[1] for e in startpoints]
+plt.plot(sx,sy,'o', c = 'black')
+
 C = Z.reshape(N,N)
 plt.pcolormesh(XX,YY,C)
 plt.title('Gamma = ' + str(gamma))
 plt.colorbar()
-plt.scatter([-1,1],[0,0],c= 'green',s=40,marker = 'x',edgecolors='green', label = 'Fix points')
+plt.scatter([-1,1],[0,0],c= 'green',s=50,marker = 'x',edgecolors='green', label = 'Fix points')
 plt.legend()
 plt.axis([-grid,grid,-grid,grid])
 plt.show()
--- ex3_duffing/plots.py
+++ ex3_duffing/plots.py
@@ -1,24 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = 1
-b = 1
-c = 1
-d = 2
+p = 3.
+b = 2.
 
-x = 3
-y = 3
+x = 2
+y = 2
+gamma = 3.
+
+points= [[p/2.-b/2.,0],[1,p/b]]
 
 X,Y = np.meshgrid(np.arange(-x,x,0.2),np.arange(-y,y,0.2))
 
-U = a*(1-X)*X-c*X*Y
-V = -b*Y+d*X*Y
+U = Y
+V = -gamma*Y+X-X**3
 
 plt.figure(1)
 plt.axis([-x,x,-y,y])
 
-plt.streamplot(X,Y,U,V,density = 4)
-
+plt.streamplot(X,Y,U,V,density=3)
+w,z = points
+plt.plot(w,z,'x')
 plt.figure(2)
 plt.quiver(X,Y,U,V,pivot = 'mid')#,width = 0.0005)
 plt.show()
